3f17a0cac1764c69ff6a86635ecbaf45176be0ff
crodrigu <crodrigu@ubuntu.ubuntu-domain>
2014-12-23 17:55:28 +0100

    Version uaclient.py: Integrado parser en el codigo

--- uaclient.py
+++ uaclient.py
@@ -7,31 +7,53 @@ Programa cliente que abre un socket a un servidor
 
 import sys
 import socket
+import parserconfig
+import time
+from xml.sax import make_parser
 
-
-# Cliente UDP simple.
-
-try:
-    #Parametros para la conexión.
-    METODO = sys.argv[1]
-    SIP = sys.argv[2]
-except IndexError:
-    print "Usage: python client.py method receiver@IP:SIPport"
+if len(sys.argv) != 4:
+    print "Usage: python server.py config metodo opcion"
     sys.exit()
+
+
+#Parametros para la conexión.
+    
+FICHEROCONFIG = sys.argv[1]
+METODO = sys.argv[2]
+OPCION = sys.argv[3]
+
+def readficheroconfig():
+    parser = make_parser()
+    ParserDTD = parserconfig.ParserDTD()
+    parser.setContentHandler(ParserDTD)
+    parser.parse(open("ua2.xml"))
+    TAGS = ParserDTD.get_tags()
+    return TAGS
+
+CONFIG ={}
+CONFIG = readficheroconfig()
 
 # Dirección IP del servidor.
 
-SIP = SIP.split("@")
-SIP[1] = SIP[1].split(":")
+USER = CONFIG["account_username"]
+SERVER = CONFIG["uaserver_ip"]
+PORT = int(CONFIG["uaserver_puerto"])
+PORT_RTP = int(CONFIG["rtpaudio_puerto"])
 
-USER = SIP[0]
-SERVER = SIP[1][0]
-PORT = int(SIP[1][1])
+SDP = "v=0\r\n" + "o=" + USER + "\r\n" + "s=misesion\r\n" + "m=audio " + str(PORT_RTP) + " RTP"
 
-ACK = "ACK" + ' ' + USER + '@' + SERVER + " SIP/2.0\r\n"
+
+ACK = "ACK" + ' sip:' + USER + '@' + SERVER + " SIP/2.0\r\n"
 
 # Contenido que vamos a enviar
-LINE = METODO + ' ' + USER + "@" + SERVER + " SIP/2.0\r\n"
+if METODO == "INVITE":
+    LINE = "INVITE" + ' sip:' + USER + "@" + SERVER + " SIP/2.0\r\n" 
+    LINE += "Content-Type: application/sdp\r\n\r\n"
+    LINE += SDP
+elif METODO == "REGISTER":
+    LINE = ""
+elif METODO == "BYE":
+    LINE = ""
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -44,7 +66,8 @@ try:
     data = my_socket.recv(1024)
     print 'Recibido -- ', data
 except socket.error:
-    print "Error: No server listening at " + SERVER + " port " + str(PORT)
+    fecha = time.strftime('%Y%m%d%H%M%S',time.gmtime(time.time()))
+    print fecha + " Error: No server listening at " + SERVER + " port " + str(PORT)
     sys.exit()
 
 data = data.split('\r\n\r\n')
