6b71160c8df345078ce3cded09897b66f3e5801e
auronff10 <ledesmax@gmail.com>
2014-12-26 00:52:01 +0100

    Empiezo haciendo xml y empiezo user agent

---
+++ uaclient.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: iso-8859-15 -*-
+"""
+Programa del user agent final
+"""
+from xml.sax import make_parser
+from xml.sax.handler import ContentHandler
+import socket
+import sys
+import uaserver
+import os
+import time
