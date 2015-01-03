847fe4521f609aae8fa6644e5f5306b7b92fb294
auronff10 <ledesmax@gmail.com>
2015-01-03 18:56:35 +0100

    Cliente finalizado, codigos de respuesta configurados

--- uaclient.py
+++ uaclient.py
@@ -150,10 +150,220 @@ if __name__ == "__main__":
         Log1.write(": ")
         Log1.write(LINE) # SOlo tenemos una linea, no hace falta separar.
         Log1.write("\r\n")
+    try:
+        my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #Igual a P6
+        my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        my_socket.connect((SERVER, PORT))
+        my_socket.send(LINE + '\r\n') # Envio de metodo y direccion SIP
+        data = my_socket.recv(1024)
+        my_socket.close()
+    except:
+        Tiempo_log = time.strftime('%Y­%m­%d %H:%M:%S', time.gmtime(time.time()))
+        Log1.write(Tiempo_log[:4]) # Bloque temporal para el caso de error
+        Log1.write(Tiempo_log[6:8])
+        Log1.write(Tiempo_log[10:12])
+        Log1.write(Tiempo_log[13:15])
+        Log1.write(Tiempo_log[16:18])
+        Log1.write(Tiempo_log[19:21])
+        Log1.write(" Error: no server listening at " + SERVER + " port " \
+        + str(PORT) + "\r\n")
+        Tiempo_log = time.strftime('%Y­%m­%d %H:%M:%S', time.gmtime(time.time()))
+        Log1.write(Tiempo_log[:4]) # Bloque temporal para la finalización
+        Log1.write(Tiempo_log[6:8])
+        Log1.write(Tiempo_log[10:12])
+        Log1.write(Tiempo_log[13:15])
+        Log1.write(Tiempo_log[16:18])
+        Log1.write(Tiempo_log[19:21])
+        Log1.write(" Finishing.")
+        Log1.close()
+        sys.exit("Error: no server listening at " + SERVER + " port " + str(PORT))
+
+    if data:
+
+        print data
+        Codigo = data.split(" ")[1] # Codigo de respuesta dado
+
+        if Codigo == "100": #  A partir del trying, el ringing es consecuente.
+            Tiempo_log = time.strftime('%Y­%m­%d %H:%M:%S', time.gmtime(time.time()))
+            Log1.write(Tiempo_log[:4])
+            Log1.write(Tiempo_log[6:8])
+            Log1.write(Tiempo_log[10:12])
+            Log1.write(Tiempo_log[13:15])
+            Log1.write(Tiempo_log[16:18])
+            Log1.write(Tiempo_log[19:21])
+            Log1.write(" Received from ")
+            Log1.write(SERVER)
+            Log1.write(":")
+            Log1.write(str(PORT))
+            Log1.write(": ")
+            Message = data.split("\r\n")
+
+            Log1.write(Message[0]) # Igual que antes, separamos los elementos.
+            Log1.write(" ")
+            Log1.write(Message[1])
+            Log1.write(" ")
+            Log1.write(Message[2])
+            Log1.write(" ")
+            Log1.write(Message[3])
+            Log1.write(" ")
+            Log1.write(Message[4])
+            Log1.write(" ")
+            Log1.write(Message[5])
+            Log1.write(" ")
+            Log1.write(Message[6])
+            Log1.write(" ")
+            Log1.write(Message[7])
+            Log1.write(" ")
+            Log1.write(Message[8])
+            Log1.write(" ")
+            Log1.write(Message[9])
+            Log1.write("\r\n")
 
 
+            my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Enviamos la linea 
+            my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            my_socket.connect((SERVER, PORT))
+            LINE = "ACK sip:" + Receiver + " SIP/2.0"
+            my_socket.send(LINE + '\r\n')
+
+            Ip1 = data.split("o=")[1] # Recortamos origen y medio.
+            Ip1 = Ip1.split(" ")[1]
+            Ip1 = Ip1.split("\r\n")[0]
+            Puerto1 = data.split("m=")[1]
+            Puerto1 = Puerto1.split(" ")[1]
+            Cancion = etiquetas[5]
+            Cancion = Cancion["path"]
+
+            Tiempo_log = time.strftime('%Y­%m­%d %H:%M:%S', time.gmtime(time.time()))
+            Log1.write(Tiempo_log[:4]) #Bloque temporal para indicar el envio
+            Log1.write(Tiempo_log[6:8])
+            Log1.write(Tiempo_log[10:12])
+            Log1.write(Tiempo_log[13:15])
+            Log1.write(Tiempo_log[16:18])
+            Log1.write(Tiempo_log[19:21])
+            Log1.write(" Sent to ")
+            Log1.write(SERVER)
+            Log1.write(":")
+            Log1.write(str(PORT))
+            Log1.write(": ")
+            Log1.write(LINE)
+            Log1.write("\r\n")
+
+            Tiempo_log = time.strftime('%Y­%m­%d %H:%M:%S', time.gmtime(time.time()))
+            Log1.write(Tiempo_log[:4]) # Bloque temporal para el envio RTP
+            Log1.write(Tiempo_log[6:8])
+            Log1.write(Tiempo_log[10:12])
+            Log1.write(Tiempo_log[13:15])
+            Log1.write(Tiempo_log[16:18])
+            Log1.write(Tiempo_log[19:21])
+            Log1.write(" Starting RTP transfer...\r\n") # Inicio del envio RTP
+            aEjecutar = "./mp32rtp -i " + Ip1 + " -p " + Puerto1
+            aEjecutar += " < " + Cancion
+            print "Vamos a ejecutar", aEjecutar
+            os.system(aEjecutar)
+            Tiempo_log = time.strftime('%Y­%m­%d %H:%M:%S', time.gmtime(time.time()))
+            Log1.write(Tiempo_log[:4]) # Acabamos la transmicion RTP
+            Log1.write(Tiempo_log[6:8])
+            Log1.write(Tiempo_log[10:12])
+            Log1.write(Tiempo_log[13:15])
+            Log1.write(Tiempo_log[16:18])
+            Log1.write(Tiempo_log[19:21])
+            Log1.write(" Finishing RTP transfer...\r\n")
+            Log1.close()
+
+        elif Codigo == "200": # Del 200 OK
+
+            Tiempo_log = time.strftime('%Y­%m­%d %H:%M:%S', time.gmtime(time.time()))
+            Log1.write(Tiempo_log[:4]) #Bloque temporal para el 200 OK
+            Log1.write(Tiempo_log[6:8])
+            Log1.write(Tiempo_log[10:12])
+            Log1.write(Tiempo_log[13:15])
+            Log1.write(Tiempo_log[16:18])
+            Log1.write(Tiempo_log[19:21])
+            Log1.write(" Received from ")
+            Log1.write(SERVER)
+            Log1.write(":")
+            Log1.write(str(PORT))
+            Log1.write(": ")
+            Log1.write(data)
+
+            Tiempo_log = time.strftime('%Y­%m­%d %H:%M:%S', time.gmtime(time.time()))
+            Log1.write(Tiempo_log[:4]) # Acabada la recepcion del 200 OK
+            Log1.write(Tiempo_log[6:8])
+            Log1.write(Tiempo_log[10:12])
+            Log1.write(Tiempo_log[13:15])
+            Log1.write(Tiempo_log[16:18])
+            Log1.write(Tiempo_log[19:21])
+            Log1.write(" Finishing.")
+            Log1.close()
 
 
+        elif Codigo == "400": # Mismo procedimiento para Bad Request
+            Tiempo_log = time.strftime('%Y­%m­%d %H:%M:%S', time.gmtime(time.time()))
+            Log1.write(Tiempo_log[:4])
+            Log1.write(Tiempo_log[6:8])
+            Log1.write(Tiempo_log[10:12])
+            Log1.write(Tiempo_log[13:15])
+            Log1.write(Tiempo_log[16:18])
+            Log1.write(Tiempo_log[19:21])
+            Log1.write(" Received from ")
+            Log1.write(SERVER)
+            Log1.write(":")
+            Log1.write(str(PORT))
+            Log1.write(": ")
+            Log1.write(data)
+            Tiempo_log = time.strftime('%Y­%m­%d %H:%M:%S', time.gmtime(time.time()))
+            Log1.write(Tiempo_log[:4])
+            Log1.write(Tiempo_log[6:8])
+            Log1.write(Tiempo_log[10:12])
+            Log1.write(Tiempo_log[13:15])
+            Log1.write(Tiempo_log[16:18])
+            Log1.write(Tiempo_log[19:21])
+            Log1.write(" Finishing.")
+            Log1.close()
 
+        elif Codigo == "405": # Igual para Method not allowed
+            Tiempo_log = time.strftime('%Y­%m­%d %H:%M:%S', time.gmtime(time.time()))
+            Log1.write(Tiempo_log[:4])
+            Log1.write(Tiempo_log[6:8])
+            Log1.write(Tiempo_log[10:12])
+            Log1.write(Tiempo_log[13:15])
+            Log1.write(Tiempo_log[16:18])
+            Log1.write(Tiempo_log[19:21])
+            Log1.write(" Received from ")
+            Log1.write(SERVER)
+            Log1.write(":")
+            Log1.write(str(PORT))
+            Log1.write(": ")
+            Log1.write(data)
+            Tiempo_log = time.strftime('%Y­%m­%d %H:%M:%S', time.gmtime(time.time()))
+            Log1.write(Tiempo_log[:4])
+            Log1.write(Tiempo_log[6:8])
+            Log1.write(Tiempo_log[10:12])
+            Log1.write(Tiempo_log[13:15])
+            Log1.write(Tiempo_log[16:18])
+            Log1.write(Tiempo_log[19:21])
+            Log1.write(" Finishing.")
+            Log1.close()
 
-
+        else: # El ultimo caso es 404 User not found
+            Tiempo_log = time.strftime('%Y­%m­%d %H:%M:%S', time.gmtime(time.time()))
+            Log1.write(Tiempo_log[:4])
+            Log1.write(Tiempo_log[6:8])
+            Log1.write(Tiempo_log[10:12])
+            Log1.write(Tiempo_log[13:15])
+            Log1.write(Tiempo_log[16:18])
+            Log1.write(Tiempo_log[19:21])
+            Log1.write(" Error: User Not Found\r\n")
+            Tiempo_log = time.strftime('%Y­%m­%d %H:%M:%S', time.gmtime(time.time()))
+            Log1.write(Tiempo_log[:4])
+            Log1.write(Tiempo_log[6:8])
+            Log1.write(Tiempo_log[10:12])
+            Log1.write(Tiempo_log[13:15])
+            Log1.write(Tiempo_log[16:18])
+            Log1.write(Tiempo_log[19:21])
+            Log1.write(" Finishing.")
+            Log1.close()
+    else:
+        Log1.write(" Finishing.")
+        Log1.close()
