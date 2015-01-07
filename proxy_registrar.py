b8dfebb2d6f5017c9f89d7af175e865b3ea701cd
auronff10 <ledesmax@gmail.com>
2015-01-07 11:15:33 +0100

    Proxy_Registrar acabado

--- proxy_registrar.py
+++ proxy_registrar.py
@@ -382,4 +382,216 @@ class EchoHandler(SocketServer.DatagramRequestHandler):  # Segun el metodo, actu
                 else:
                     self.wfile.write(
                         "SIP/2.0 400 Bad Request\r\n")  # Mal formado
+            elif Metodo == "ACK":
 
+                Iter = 0
+                for User in UsuariosReg:  # Revisamos expiracion
+                    if float(UsuariosReg[Iter][3]) + \
+                            float(UsuariosReg[Iter][4]) < time.time():
+
+                        del UsuariosReg[Iter]
+                    Iter = Iter + 1
+                Aim = line.split(" ")[1]  # Para sacar Puerto e IP
+                Aim = Aim.split(":")[1]
+                Log1 = etiquetas[2]
+                Log1 = Log1["path"]
+                Log1 = open(Log1, "a")
+                for User in UsuariosReg:
+                    if User[0] != Aim:
+                        IP_origen = User[1]
+                        Puerto_origen = User[2]
+                Tiempo_log = time.strftime('%Y­%m­%d %H:%M:%S',
+                                           time.gmtime(time.time()))
+                Log1.write(Tiempo_log[:4])
+                Log1.write(Tiempo_log[6:8])
+                Log1.write(Tiempo_log[10:12])
+                Log1.write(Tiempo_log[13:15])
+                Log1.write(Tiempo_log[16:18])
+                Log1.write(Tiempo_log[19:21])
+                Log1.write(" Received from ")  # De quien viene el ACK
+                Log1.write(IP_origen)
+                Log1.write(":")
+                Log1.write(Puerto_origen)
+                Log1.write(": ")
+                Log1.write(line)
+                Iter = 0
+                for User in UsuariosReg:
+                    if UsuariosReg[Iter][0] == Aim:
+                        SERVER = UsuariosReg[Iter][1]
+                        PORT = int(UsuariosReg[Iter][2])
+
+                        my_socket = socket.socket(socket.AF_INET,
+                                                  socket.SOCK_DGRAM)  # Enviamos el ACK
+                        my_socket.setsockopt(socket.SOL_SOCKET,
+                                             socket.SO_REUSEADDR, 1)
+                        my_socket.connect((SERVER, PORT))
+                        my_socket.send(line)
+
+                        data = my_socket.recv(1024)
+
+                        my_socket.close()
+                        Tiempo_log = time.strftime('%Y­%m­%d %H:%M:%S',
+                                                   time.gmtime(time.time()))
+                        Log1.write(
+                            Tiempo_log[:4])  # Indicamos el envio del ACK
+                        Log1.write(Tiempo_log[6:8])
+                        Log1.write(Tiempo_log[10:12])
+                        Log1.write(Tiempo_log[13:15])
+                        Log1.write(Tiempo_log[16:18])
+                        Log1.write(Tiempo_log[19:21])
+                        Log1.write(" Sent to ")
+                        Log1.write(SERVER)
+                        Log1.write(":")
+                        Log1.write(str(PORT))
+                        Log1.write(": ")
+                        Log1.write(line)
+                    Iter = Iter + 1
+                Log1.close()
+
+            elif Metodo == "BYE":
+
+                Iter = 0
+                for User in UsuariosReg:  # Revisamos expiracion
+                    if float(UsuariosReg[Iter][3]) + \
+                            float(UsuariosReg[Iter][4]) < time.time():
+                        del UsuariosReg[Iter]
+                    Iter = Iter + 1
+                Aim = line.split(" ")[1]
+                Aim = Aim.split(":")[1]
+                Log1 = etiquetas[2]
+                Log1 = Log1["path"]
+                Log1 = open(Log1, "a")
+                Tiempo_log = time.strftime('%Y­%m­%d %H:%M:%S',
+                                           time.gmtime(time.time()))
+                Log1.write(Tiempo_log[:4])
+                Log1.write(Tiempo_log[6:8])
+                Log1.write(Tiempo_log[10:12])
+                Log1.write(Tiempo_log[13:15])
+                Log1.write(Tiempo_log[16:18])
+                Log1.write(Tiempo_log[19:21])
+                Found = False  # Revisamos usuarios
+                for User in UsuariosReg:
+                    if User[0] != Aim:
+                        IP_origen = User[1]
+                        Puerto_origen = User[2]
+                        Found = True
+                if Found:
+                    Log1.write(" Received from ")  # Iniciador del BYE
+                    Log1.write(IP_origen)
+                    Log1.write(":")
+                    Log1.write(Puerto_origen)
+                    Log1.write(": ")
+                    Log1.write(line)
+                    Iter = 0
+                    for User in UsuariosReg:
+                        if UsuariosReg[Iter][0] == Aim:
+
+                            SERVER = UsuariosReg[Iter][1]
+                            PORT = int(UsuariosReg[Iter][2])
+
+                            my_socket = socket.socket(socket.AF_INET,
+                                                      socket.SOCK_DGRAM)
+                            my_socket.setsockopt(socket.SOL_SOCKET,
+                                                 socket.SO_REUSEADDR, 1)
+                            my_socket.connect((SERVER, PORT))
+                            my_socket.send(line)  # Envio del BYE
+                            data = my_socket.recv(1024)
+                            print data
+                            my_socket.close()
+                            self.wfile.write(data)
+
+                            Tiempo_log = time.strftime('%Y­%m­%d %H:%M:%S',
+                                                       time.gmtime(time.time()))
+                            Log1.write(
+                                Tiempo_log[:4])  # EL que se envia al servidor
+                            Log1.write(Tiempo_log[6:8])
+                            Log1.write(Tiempo_log[10:12])
+                            Log1.write(Tiempo_log[13:15])
+                            Log1.write(Tiempo_log[16:18])
+                            Log1.write(Tiempo_log[19:21])
+                            Log1.write(" Sent to ")
+                            Log1.write(SERVER)
+                            Log1.write(":")
+                            Log1.write(str(PORT))
+                            Log1.write(": ")
+                            Log1.write(line)
+                            Tiempo_log = time.strftime('%Y­%m­%d %H:%M:%S',
+                                                       time.gmtime(time.time()))
+                            Log1.write(Tiempo_log[:4])
+                            Log1.write(
+                                Tiempo_log[6:8])  # Una vez recibe del servidor
+                            Log1.write(Tiempo_log[10:12])
+                            Log1.write(Tiempo_log[13:15])
+                            Log1.write(Tiempo_log[16:18])
+                            Log1.write(Tiempo_log[19:21])
+                            Log1.write(" Received from ")
+                            Log1.write(SERVER)
+                            Log1.write(":")
+                            Log1.write(str(PORT))
+                            Log1.write(": ")
+                            Log1.write(data)
+                            Tiempo_log = time.strftime('%Y­%m­%d %H:%M:%S',
+                                                       time.gmtime(time.time()))
+                            Log1.write(Tiempo_log[:4])
+                            Log1.write(Tiempo_log[6:8])  # Envio del cliente
+                            Log1.write(Tiempo_log[10:12])
+                            Log1.write(Tiempo_log[13:15])
+                            Log1.write(Tiempo_log[16:18])
+                            Log1.write(Tiempo_log[19:21])
+                            Log1.write(" Sent to ")
+                            Log1.write(IP_origen)
+                            Log1.write(":")
+                            Log1.write(Puerto_origen)
+                            Log1.write(": ")
+                            Log1.write(data)
+                        Iter = Iter + 1
+                    Log1.close()
+                else:  # NO esta en la database
+                    Log1.write(" Error: User Not Found\r\n")
+                    self.wfile.write("SIP/2.0 404 User Not Found\r\n")
+                    Log1.close()
+            elif Metodo != "REGISTER" and Metodo != "INVITE" \
+                    and Metodo != "BYE" and Metodo:
+                self.wfile.write("SIP/2.0 405 Method Not Allowed\r\n")
+                                 # No hay mas metodos soportados.
+            if not line:
+                break
+if __name__ == "__main__":
+
+    if len(sys.argv) != 2:  # COmprobamos el numero de elementos introducidos.
+        sys.exit("Usage: python proxy_registrar.py config")
+
+    parser = make_parser()  # Para sacar las etiquetas
+    cHandler = SessionHandler()
+    parser.setContentHandler(cHandler)
+    etiquetas = cHandler.get_tags()
+    config = sys.argv[1]
+
+    try:  # Comprobamos que sea leible el config lo primero.
+        parser.parse(open(config))
+    except:
+        sys.exit("Usage: python proxy_registrar.py config")
+
+    Log1 = etiquetas[2]
+    Log1 = Log1["path"]
+    Log1 = open(Log1, "a")  # Log del proxy
+    Log1.write(
+        "_____________________________________________________" + "\r\n")
+               # Para diferenciar
+    Tiempo_log = time.strftime('%Y­%m­%d %H:%M:%S', time.gmtime(time.time()))
+    Log1.write(Tiempo_log[:4])  # Inicio del programa proxy
+    Log1.write(Tiempo_log[6:8])
+    Log1.write(Tiempo_log[10:12])
+    Log1.write(Tiempo_log[13:15])
+    Log1.write(Tiempo_log[16:18])
+    Log1.write(Tiempo_log[19:21])
+    Log1.write(" Starting...\r\n")
+    Log1.close()
+    Direccion = etiquetas[0]
+    Usuario = Direccion["name"]
+    Ip = Direccion["ip"]
+    Puerto = Direccion["puerto"]
+    UsuariosReg = []  # Lista para la database de los usuarios registrados
+    serv = SocketServer.UDPServer((Ip, int(Puerto)), EchoHandler)
+    print "Server " + Usuario + " listening at port " + Puerto + "..."
+    serv.serve_forever()
